772725e2fb9b4d2425c0bb2db1f9f87e81ad7ade
Raphael <raphael@Terror.(none)>
2011-11-22 10:02:11 +0100

    Remise des Places dans l'ordre alphabétique... (sinon on s'y perd un peu :/ )

--- src/fr/piroxxi/s2le/client/ApplicationPlaceHistoryMapper.java
+++ src/fr/piroxxi/s2le/client/ApplicationPlaceHistoryMapper.java
@@ -3,23 +3,26 @@ package fr.piroxxi.s2le.client;
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 import com.google.gwt.place.shared.WithTokenizers;
 
+import fr.piroxxi.s2le.client.places.CreateAccountConfirmationPlace;
 import fr.piroxxi.s2le.client.places.CreateAccountPlace;
-import fr.piroxxi.s2le.client.places.EditNewQuestionPlace;
-import fr.piroxxi.s2le.client.places.StartQuestionCreationPlace;
 import fr.piroxxi.s2le.client.places.CreateTestPlace;
+import fr.piroxxi.s2le.client.places.EditNewQuestionPlace;
 import fr.piroxxi.s2le.client.places.EditQuestionPlace;
 import fr.piroxxi.s2le.client.places.ErrorPlace;
 import fr.piroxxi.s2le.client.places.HelloPlace;
 import fr.piroxxi.s2le.client.places.ListeQuestionsPlace;
 import fr.piroxxi.s2le.client.places.ResultTestPlace;
+import fr.piroxxi.s2le.client.places.StartQuestionCreationPlace;
 import fr.piroxxi.s2le.client.places.TestPlace;
 
-@WithTokenizers({ HelloPlace.Tokenizer.class,
-		ListeQuestionsPlace.Tokenizer.class, CreateTestPlace.Tokenizer.class,
-		TestPlace.Tokenizer.class, ResultTestPlace.Tokenizer.class,
-		StartQuestionCreationPlace.Tokenizer.class,
-		EditQuestionPlace.Tokenizer.class, ErrorPlace.Tokenizer.class,
+@WithTokenizers({ CreateAccountConfirmationPlace.Tokenizer.class,
+		CreateAccountPlace.Tokenizer.class, CreateTestPlace.Tokenizer.class,
 		EditNewQuestionPlace.Tokenizer.class,
-		CreateAccountPlace.Tokenizer.class })
+		EditQuestionPlace.Tokenizer.class, ErrorPlace.Tokenizer.class,
+		HelloPlace.Tokenizer.class, ListeQuestionsPlace.Tokenizer.class,
+		ResultTestPlace.Tokenizer.class, TestPlace.Tokenizer.class,
+		StartQuestionCreationPlace.Tokenizer.class,
+
+})
 public interface ApplicationPlaceHistoryMapper extends PlaceHistoryMapper {
 }
